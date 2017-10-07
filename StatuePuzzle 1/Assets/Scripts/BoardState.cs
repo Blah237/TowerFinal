a0aa1ea2b3b411480d6038cf49adb04e11c5ed52
Reid Wade <reid.m.wade@gmail.com>
2017-10-07 16:28:20 -0400

    Stub for refactored BoardState, want to wait on this until laser data rep is merged in

---
+++ StatuePuzzle 1/Assets/Scripts/BoardState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardState : MonoBehaviour
+{
+
+	public BoardCodes[,] staticDefs;
+	public coord playerPosition;
+	public HashSet<coord> mimicPositions;
+	public HashSet<coord> mirrorPositions;
+	public Dictionary<coord,bool> buttonStates;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
