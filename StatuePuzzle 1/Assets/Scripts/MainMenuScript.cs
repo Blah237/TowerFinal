aa55b67c5fc546f7c8dd9d3447ed8b02ac4cc094
Graham Thomas Rutledge <gtr34@cornell.edu>
2017-11-19 17:35:40 -0500

    credits, mailing, privacy

---
+++ StatuePuzzle 1/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+    [SerializeField]
+    RawImage privacy;
+    [SerializeField]
+    RawImage mailing;
+    [SerializeField]
+    RawImage credits;
+
+    public void showPrivacy() {
+        //privacy.enabled = true;
+        privacy.gameObject.SetActive(true);
+        Debug.Log("Show Privacy");
+    }
+
+    public void hidePrivacy() {
+        privacy.gameObject.SetActive(false);
+        Debug.Log("Hide Privacy");
+    }
+
+    public void showMailing() {
+        mailing.gameObject.SetActive(true);
+        Debug.Log("Show Mailing");
+    }
+
+    public void hideMailing() {
+        mailing.gameObject.SetActive(false);
+        Debug.Log("Hide Mailing");
+    }
+
+    public void submitMailing() {
+        string email = mailing.transform.Find("InputField").GetComponent<InputField>().text;
+        LoggingManager.instance.RecordEmail(email);
+        hideMailing();
+    }
+
+    public void showCredits() {
+        credits.gameObject.SetActive(true);
+        Debug.Log("Show Credits");
+    }
+
+    public void hideCredits() {
+        credits.gameObject.SetActive(false);
+        Debug.Log("Hide Credits");
+    }
+}
